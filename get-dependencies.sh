1a9eb5aac0745bd89d29a95023e2b0fa223e079c
Link4Electronics <link4electronics@gmail.com>
2026-02-11 11:40:27 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,9 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-    wget \
-    jre-openjdk-headless #java-runtime-headless
+pacman -Syu --noconfirm jre-openjdk-headless
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
