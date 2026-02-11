d963456f1689841289cfee645c3de2a20dc09912
Link4Electronics <link4electronics@gmail.com>
2026-02-11 10:42:11 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,6 +19,8 @@ get-debloated-pkgs --add-common --prefer-nano
 #mv -v /srv/papermc/* ./AppDir/bin
 
 # If the application needs to be manually built that has to be done down here
+archlinux-java status
+archlinux-java set java-25-openjdk
 wget --retry-connrefused --tries=30 "https://api.papermc.io/v2/projects/paper/versions/1.21.11/builds/69/downloads/paper-1.21.11-69.jar" -O ./AppDir/bin/papermc_server.jar
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
