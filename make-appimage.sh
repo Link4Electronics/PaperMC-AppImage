31aa090188509452604c0dffe1ba623401247b39
Link4Electronics <link4electronics@gmail.com>
2026-02-11 11:39:31 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,10 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/PaperMC/Paper/refs/heads/main/paper-server/src/main/resources/logo.png
 #export DESKTOP=DUMMY
-#export JAVA_HOME=/usr/lib/jvm/java-25-openjdk
-#export PATH=$JAVA_HOME/bin:$PATH
 
-#archlinux-java set java-25-openjdk
 # Deploy dependencies
 #quick-sharun /usr/lib/jvm/java-25-openjdk/bin/java -jar /srv/papermc/papermc_server.jar #/usr/lib/jvm/java-25-openjdk/lib/*
 
