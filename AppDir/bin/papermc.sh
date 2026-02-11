5a3f2dc036923ffd8c55029cf21c454ca8885a99
Link4Electronics <link4electronics@gmail.com>
2026-02-11 12:00:58 -0300

    Update papermc.sh

--- AppDir/bin/papermc.sh
+++ AppDir/bin/papermc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec java -Xms512M -Xmx1024M -Dcom.mojang.eula.agree=true -jar papermc_server.jar nogui "$@"
+exec java -Xms512M -Xmx1024M -Dcom.mojang.eula.agree=true -jar ./AppDir/bin/papermc_server.jar nogui "$@"
