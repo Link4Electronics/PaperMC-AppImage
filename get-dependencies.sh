da850f5fbf8faf535f3e49b8187567b7521138da
Link4Electronics <link4electronics@gmail.com>
2026-02-11 10:39:07 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,18 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm jre-openjdk-headless #java-runtime-headless
+pacman -Syu --noconfirm \
+    wget \
+    jre-openjdk-headless #java-runtime-headless
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package papermc
+#make-aur-package papermc
 #mv -v /srv/papermc/* ./AppDir/bin
 
 # If the application needs to be manually built that has to be done down here
-
+wget --retry-connrefused --tries=30 "https://api.papermc.io/v2/projects/paper/versions/1.21.11/builds/69/downloads/paper-1.21.11-69.jar" -O ./AppDir/bin/papermc_server.jar
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
 # if [ "${DEVEL_RELEASE-}" = 1 ]; then
