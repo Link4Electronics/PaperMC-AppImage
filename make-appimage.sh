bfef8ce831a2ed1b980542fd4abae1955365a5b6
Link4Electronics <link4electronics@gmail.com>
2026-02-11 11:00:32 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -21,7 +21,7 @@ export ICON=https://raw.githubusercontent.com/PaperMC/Paper/refs/heads/main/pape
 
 quick-sharun \
         ./AppDir/bin/* \
-         /usr/lib/jvm/java-25-openjdk/*
+         /usr/lib/jvm/java-25-openjdk/bin/*
 chmod +x ./AppDir/bin/papermc.sh
 
 # Additional changes can be done in between here
